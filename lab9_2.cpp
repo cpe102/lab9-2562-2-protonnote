ea6253dddf18a3d67946587991d587bc804e8349
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-28 16:20:02 +0700

    send1

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include<fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream cheer;
+    ofstream cheerbook;
+    cheer.open("cheerbook.txt");
+    cheerbook.open("cheerbook_copy.txt");
+    string textline;
+    cheerbook << "-------------------- SOTUS ---------------------" << endl;
+    bool counttext;
+        counttext = getline(cheer,textline);
+        getline(cheer,textline);
+    while (counttext){
+        cheerbook << textline << endl;
+        counttext = getline(cheer,textline);
+    }
+    cheerbook << "-------------------- SOTUS ---------------------";
+    return 0;
+}
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>         //cout << "Number of data = ";
+#include <fstream>           //cout << "Number of data = ";
+#include <string>           //cout << "Mean = ";
+#include <cmath>            //<< "Standard deviation = ";
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open("score.txt");
+    string textline;
+    double sum = 0, mean,sd,num=0;
+    while(getline(score,textline)){
+        sum += atof(textline.c_str());
+        num++;  
+    }
+    cout << "Number of data = " << num << endl;
+    mean = 1/num*sum;
+    cout << "Mean = " << mean << endl;
+    sd = pow((pow(sum,2)/num)-pow(mean,2),0.5);
+    cout << "Standard deviation = " << sd << endl;
+    return 0;
+}
+    
+    
+    
+
+
